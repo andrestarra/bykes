52f36bd0a01b213ab70c8558ee2fdb9730c915b6
AndresFTR <afelipe0113@gmail.com>
2019-05-31 11:41:38 -0500

    Method in model that generate a unique code from rental.

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,4 +1,18 @@
 class Rental < ApplicationRecord
   belongs_to :user
-  belongs_to :station
+  before_save :unique_code
+
+  protected
+
+  def generate_code(number)
+    charset = Array('A'..'Z') + Array('a'..'z') + Array(0..9)
+    Array.new(number) { charset.sample }.join
+  end
+
+  def unique_code
+    self.code = loop do
+      random_code = generate_code(6)
+      break random_code unless Rental.exists?(code: random_code)
+    end
+  end
 end
