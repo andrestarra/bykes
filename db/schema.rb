72a2fbc8db6d196d689b4be71e5be8af5c58abe1
AndresFTR <afelipe0113@gmail.com>
2019-06-04 14:35:48 -0500

    Model admin created.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_30_212700) do
+ActiveRecord::Schema.define(version: 2019_06_04_184555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.integer "failed_attempts", default: 0, null: false
+    t.string "unlock_token"
+    t.datetime "locked_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["unlock_token"], name: "index_admins_on_unlock_token", unique: true
+  end
 
   create_table "bikes", force: :cascade do |t|
     t.string "serial_number"
@@ -22,6 +39,18 @@ ActiveRecord::Schema.define(version: 2019_05_30_212700) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["station_id"], name: "index_bikes_on_station_id"
+  end
+
+  create_table "records", force: :cascade do |t|
+    t.datetime "ends_at"
+    t.bigint "rental_id"
+    t.bigint "bike_id"
+    t.bigint "station_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["bike_id"], name: "index_records_on_bike_id"
+    t.index ["rental_id"], name: "index_records_on_rental_id", unique: true
+    t.index ["station_id"], name: "index_records_on_station_id"
   end
 
   create_table "rentals", force: :cascade do |t|
@@ -58,5 +87,8 @@ ActiveRecord::Schema.define(version: 2019_05_30_212700) do
   end
 
   add_foreign_key "bikes", "stations"
+  add_foreign_key "records", "bikes"
+  add_foreign_key "records", "rentals"
+  add_foreign_key "records", "stations"
   add_foreign_key "rentals", "users"
 end
