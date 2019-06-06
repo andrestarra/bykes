46b788d4f534bed1e2cd83caf6c1ec142dd5f3ba
AndresFTR <afelipe0113@gmail.com>
2019-06-06 17:13:43 -0500

    Gem AASM to set state of bike added.

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,4 +1,22 @@
 class Bike < ApplicationRecord
+  include AASM
+
   has_many :records
   belongs_to :station
+
+  validates :serial_number, presence: true
+  validates :state, presence: true
+
+  aasm do
+    state :working, initial: true
+    state :maintenance
+
+    event :in_maintenance do
+      transitions from: :working, to: :maintenance
+    end
+
+    event :put_to_work do
+      transitions from: :maintenance, to: :working
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   resources :stations, only: [:index, :show] do
     resources :rentals
   end
+  resources :records
 end
