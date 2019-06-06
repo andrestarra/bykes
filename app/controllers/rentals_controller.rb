bd15ff4f00070199fbf0257d0005ba6f9b15458d
AndresFTR <afelipe0113@gmail.com>
2019-06-06 11:28:22 -0500

    Create rental redirect to show view since render.

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,11 +1,16 @@
 class RentalsController < ApplicationController
+  def show
+    @station = Station.find(params[:station_id])
+    @rental = @station.rentals.find(params[:id])
+  end
+
   def create
     @station = Station.find(params[:station_id])
     @rental = @station.rentals.build(rental_params)
     @rental.user_id = current_user.id
 
     if @rental.save
-      redirect_to @rental, success: 'Rental was successfully!'
+      redirect_to station_rental_path(@station, @rental), success: 'Rental was successfully!'
     else
       render 'new'
     end
