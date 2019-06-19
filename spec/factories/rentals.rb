77fbce88131f705540e8b9bc21306e5a29a88a24
AndresFTR <afelipe0113@gmail.com>
2019-06-19 11:49:10 -0500

    Rental factory and Rental model spec test.

--- spec/factories/rentals.rb
+++ spec/factories/rentals.rb
@@ -1,5 +1,10 @@
+require 'faker'
+
 FactoryBot.define do
   factory :rental do
-    
+    code { Faker::Alphanumeric.unique.alphanumeric(6) }
+    hours { rand(24) }
+    station
+    user
   end
 end
--- spec/models/rental_spec.rb
+++ spec/models/rental_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Rental, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    subject { build(:rental) }
+
+    it { should belong_to(:user).class_name('User') }
+    it { should belong_to(:station).class_name('Station') }
+    it { should have_one(:record).class_name('Record') }
+  end
+
+  describe 'validations' do
+    subject { build(:rental) }
+
+    it { should validate_uniqueness_of(:code) }
+    it { should validate_presence_of(:hours) }
+    it do
+      should validate_numericality_of(:hours).
+        is_greater_than(0).is_less_than(25)
+    end
+  end
 end
