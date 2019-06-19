6539277c6fe52a7c5ec15f1cf705b7cae79b4b78
AndresFTR <afelipe0113@gmail.com>
2019-06-19 11:19:58 -0500

    Bike factory and Bike model spec test.

--- spec/factories/bikes.rb
+++ spec/factories/bikes.rb
@@ -1,5 +1,9 @@
+require 'faker'
+
 FactoryBot.define do
   factory :bike do
-    
+    serial_number { Faker::Alphanumeric.unique.alphanumeric(6) }
+    state { 'available' }
+    station
   end
 end
--- spec/models/bike_spec.rb
+++ spec/models/bike_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Bike, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    subject { build(:bike) }
+
+    it { should have_many(:records).class_name('Record') }
+    it { should belong_to(:station).class_name('Station') }
+  end
+
+  describe 'validations' do
+    subject { build(:bike) }
+
+    it { should validate_presence_of(:serial_number) }
+    it { should validate_uniqueness_of(:serial_number) }
+    it { should validate_length_of(:serial_number).is_equal_to(6) }
+    it { should validate_presence_of(:state) }
+  end
 end
