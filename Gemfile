bf54364bbce43988a651e522ec1c94b456784fc7
andres <afelipe0113@gmail.com>
2019-06-22 22:52:43 -0500

    Database seed added.

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby '2.6.0'
+ruby '2.6.3'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
@@ -76,3 +76,4 @@ gem 'rails_admin_aasm'
 gem 'rolify'
 gem 'rubocop', '~> 0.70.0', require: false
 gem 'simple_form'
+gem 'wdm', '>= 0.1.0' if Gem.win_platform?
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(first_name: 'Fist', last_name: 'Test', identification: '1234567890', address: 'Street 85 # 45', email: 'first@test.com', password:'123abc')
+User.create(first_name: 'Second', last_name: 'Test', identification: '9876543210', address: 'Street 85 # 45', email: 'second@test.com', password:'456abc')
+station_1 = Station.create(address: 'Street 85 # 45')
+station_2 = Station.create(address: 'Street 45 # 85')
+Bike.create(serial_number: '123abc', station_id: station_1.id )
+Bike.create(serial_number: '456def', station_id: station_1.id )
+Bike.create(serial_number: '789ghi', station_id: station_2.id )
